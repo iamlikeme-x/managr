808c1ca7d9fa17bf8f5ee9dd21411a0d97748247
iamlikeme <oliver.scase1@btinternet.com>
2016-04-05 20:25:11 +0100

    Seperated photo.js into two seperate files

--- src/js/loadPhoto.js
+++ src/js/loadPhoto.js
@@ -1,11 +1,3 @@
-function loadPhotoInfo(data) {
-    console.log(data);
-    $('.imgInfo #title').html(data.photo.title._content);
-    $('.imgInfo #desc').html(data.photo.description._content.replace(/\n/g, "<br>"));
-    $('.imgInfo #user').html(data.photo.owner.username);
-    $('.imgInfo').fadeIn();
-}
-
 //"Function to be called" - Decided to keep Jake's description of this function in here...
 
 function loadRecentPhotos(pageNum) {
