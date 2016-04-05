9df663f770db49f6a1c830926d2250d32db2e3fb
iamlikeme <oliver.scase1@btinternet.com>
2016-04-05 20:48:19 +0100

    Combined files again :|

--- src/js/photoInfo.js
+++
@@ -1,7 +0,0 @@
-function loadPhotoInfo(data) {
-    console.log(data);
-    $('.imgInfo #title').html(data.photo.title._content);
-    $('.imgInfo #desc').html(data.photo.description._content.replace(/\n/g, "<br>"));
-    $('.imgInfo #user').html(data.photo.owner.username);
-    $('.imgInfo').fadeIn();
-}
