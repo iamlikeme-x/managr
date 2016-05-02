8cbb0cbba2e18c8289e85642a89a2a43e322b6ff
iamlikeme <oliver.scase1@btinternet.com>
2016-05-02 16:10:26 +0100

    Started compiling some styling for user profile

--- src/js/main.js
+++ src/js/main.js
@@ -45,6 +45,14 @@ document.onkeydown = function (pressed) {
 //END Node Webkit Stuff              //
 ///////////////////////////////////////
 
+
+function autoHeightAnimate(element, time){
+  	var curHeight = element.height(), // Get Default Height
+    autoHeight = element.css('height', 'auto').height(); // Get Auto Height
+    element.height(curHeight); // Reset to Default Height
+    element.stop().animate({ height: autoHeight }, parseInt(time)); // Animate to Auto Height
+}
+
 function checkAtts(a, obj) {
     // a: attributes to check.
     // obj: object to check against.
